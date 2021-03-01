12432805ca7e9896015f05b0902e6cdada999908
Anikait <50035624+anikait1@users.noreply.github.com>
2021-03-01 15:34:34 +0530

    Added cleaner for amazon product pages

---
+++ src/utils/cleaners/amazon-cleaner.ts
@@ -0,0 +1,29 @@
+import { ProductIdPath, urlCleaner } from "../../@types/types";
+
+const amazonCleaner: urlCleaner = (url) => {
+  const baseUrl = url.origin;
+  const productId = getProductIdPath(url.pathname);
+
+  if (productId.success === false) {
+    return "";
+  } else {
+    return baseUrl + "/dp/" + productId.path;
+  }
+};
+
+const removeExtraQueryParameters = () => {
+  throw new Error("To be implemented");
+}
+
+const getProductIdPath = (urlPath: string): ProductIdPath => {
+  const pattern = new RegExp(/gp\/product\/([A-Z0-9]{10})|dp\/([A-Z0-9]{10})/);
+  const result = urlPath.match(pattern);
+
+  if (result !== null) {
+    return { success: true, path: result[1] ?? result[2] };
+  } else {
+    return { success: false, path: null };
+  }
+};
+
+export default amazonCleaner;

7f6275ac6e94170308b12a0308a650447098b6b1
Anikait <anikait.makkar@outlook.com>
2021-03-13 18:34:56 +0530

    Added the new cleaners

--- src/utils/domain-mapper.ts
+++ src/utils/domain-mapper.ts
@@ -1,11 +1,14 @@
 import { tdomainMapper } from "../@types/types";
-import amazonCleaner from "./cleaners/amazon-cleaner";
-import flipkartCleaner from "./cleaners/flipkart-cleaner";
-
+import { amazonCleaner } from "./cleaners/amazon-cleaner";
+import { flipkartCleaner } from "./cleaners/flipkart-cleaner";
+import { ebayCleaner } from "./cleaners/ebay-cleaner";
+import { walmartCleaner } from "./cleaners/walmart-cleaner";
 
 const domainMapper: tdomainMapper = {
   "amazon": amazonCleaner,
   "flipkart": flipkartCleaner,
-}
+  "ebay": ebayCleaner,
+  "walmart": walmartCleaner,
+};
 
 export default domainMapper;
